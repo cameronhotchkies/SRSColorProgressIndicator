bc3c6ea6e450eaae2cda980346ec63178613af26
Handsome Cam <handsomecam@semisafe.com>
2012-07-24 16:58:02 -0500

    Removed a whole bunch of sublayer related code not being used.

--- SRSColorProgressIndicatorView.m
+++ SRSColorProgressIndicatorView.m
@@ -28,8 +28,6 @@
 
 @interface SRSColorProgressIndicatorView ()
     
-- (CALayer*)generateSpinnerLayerForIndex:(NSInteger)index;
-
 @end
 
 @implementation SRSColorProgressIndicatorView
@@ -64,33 +62,13 @@
 
 -(void)animate:(NSTimer *)aTimer
 {
-    [[self.layer.sublayers objectAtIndex:layerIndex] setOpacity:0];
-    [[self.layer.sublayers objectAtIndex:layerIndex] setNeedsDisplay];
-
     layerIndex += 1;
-    if (layerIndex >= 30)
+    if (layerIndex >= 12)
     {
         layerIndex = 0;
     }
     
-    [[self.layer.sublayers objectAtIndex:layerIndex] setOpacity:1];
-    //[[self.layer.sublayers objectAtIndex:layerIndex] setNeedsDisplay];
     [self.layer setNeedsDisplay];
-}
-
-- (void)drawRect:(NSRect)dirtyRect
-{
-    if (self.isSpinning)
-    {
-        // Drawing code here.
-        [self drawInteriorWithFrame:self.frame inView:self];
-    }
-    
-    NSArray* sl = self.layer.sublayers;
-    for (CALayer* cl in sl)
-    {
-        [cl setNeedsDisplay];
-    }
 }
 
 - (NSColor*)color
@@ -111,17 +89,7 @@
          alpha:&alphaComponent];
         
 		NSAssert((alphaComponent > 0.999), @"color must be opaque");
-        
-        if ([self.layer.sublayers count] > 0)
-        {
-            self.layer.sublayers = nil; 
-        }
-        
-        for (int i=0; i < 30; i++)
-        {
-            CALayer* thisLayer = [self generateSpinnerLayerForIndex:i];
-            [self.layer addSublayer:thisLayer];
-        }
+
 	}
 }
 
@@ -138,11 +106,7 @@
     {
         layerIndex = 0;
         self.layer.hidden = NO;
-        for (CALayer* slayer in self.layer.sublayers)
-        {
-            slayer.opacity = 0;
-            //            [slayer setNeedsDisplay];
-        }
+
         [self.layer setNeedsDisplay];
         
         if (self.animationTimer == nil || [self.animationTimer isValid] == NO)
@@ -164,36 +128,7 @@
         self.animationTimer = nil;
         self.layer.hidden = YES;
         [self.layer setNeedsDisplay];
-        for (CALayer* slayer in self.layer.sublayers)
-        {
-            slayer.opacity = 0;
-//            [slayer setNeedsDisplay];
-        }
     }
-}
-
-- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
-{
-	// cell has no border
-	[self drawInteriorWithFrame:cellFrame inView:controlView];
-}
-
-- (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView
-{
-    
-}
-
-- (CALayer*)generateSpinnerLayerForIndex:(NSInteger)index;
-{
-    CALayer* retVal = [[CALayer alloc] init];
-    
-    retVal.delegate = self;
-    retVal.needsDisplayOnBoundsChange=YES;
-    [retVal setOpacity:0];
-    
-//    [retVal setNeedsDisplay];
-
-    return retVal;
 }
 
 - (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx
